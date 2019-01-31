fb810c2961f801c882a71123730b89eba57eb64f
dcjones1 <dcejjones@gmail.com>
2019-01-30 23:38:24 -0500

    working stylized task list

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,46 @@
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    redirect '/tasks'
+  end
+
+  get '/tasks' do
+    @tasks = Task.all
+    erb :index
+  end
+
+  post '/tasks' do
+    @task = Task.create(params[:tasks])
+    redirect "/tasks/#{@task.id}"
+  end
+
+  get '/tasks/new' do
+    erb :new
+  end
+
+  get '/tasks/:id' do
+    @task = Task.find(params[:id])
+    erb :show
+  end
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  patch '/tasks/:id' do
+    @task = Task.find(params[:id])
+    @task.update(params[:tasks])
+    redirect "/tasks/#{@task.id}"
+  end
+
+  delete '/tasks/:id' do
+    Task.find(params[:id]).delete
+    redirect '/tasks'
+  end
+
 end
